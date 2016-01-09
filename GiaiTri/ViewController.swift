67bd549e09186abbf2c93792fb10134709f26af5
Quảng Khương Duy <quangkhuongduy@quangs-macbook-pro.local>
2016-01-09 23:29:40 +0700

    Initial Commit

---
+++ GiaiTri/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GiaiTri
+//
+//  Created by Quảng Khương Duy on 1/9/16.
+//  Copyright © 2016 Com.QuangKhuongDuy.GiaiTri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
